590282d8a2c1628dab55f0d56568f2028b12294e
TonyS <tonysilvasd@gmail.com>
2018-12-11 15:36:55 -0800

    opens blank tab when google maps button clicked

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -5,7 +5,7 @@ export default props => (
     <div className='well'>
     <h4>{props.name}</h4>
     <p>{props.description}</p>
-    <a href={'https://maps.google.com/?q=' + props.location[0] + ',' + props.location[1]}><button className='btn btn-primary'>Open in Google Maps</button></a>
+    <a href={'https://maps.google.com/?q=' + props.location[0] + ',' + props.location[1]} target='_blank'><button className='btn btn-primary'>Open in Google Maps</button></a>
     </div>
-    
+
 );
